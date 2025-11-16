9be3a3db161d8f7bed65153bf3d1131508fa9df6
unknown <jangjaeko45@gmail.com>
2025-11-15 23:06:59 -0800

    feat : es modlue system

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // const moduleData = require("./math");
-const { add, sub } = require("./math");
+// const { add, sub } = require("./math"); // only common js
+
+// import mul from "./math.js"; // es6 module
+import mul, { add, subtract } from "./math.js"; // es6 module
+
+
 // console.log(moduleData) ; // { add: [Function: add], sub: [Function: subtract] }
 
 // console.log(moduleData.add(2,3)); // 5
 // console.log(moduleData.sub(5,3)); // 2
+
 console.log(add(2, 3)); // 5
-console.log(sub(5, 3)); // 2
+console.log(subtract(5, 3)); // 2
+console.log(mul(2, 3)); // 6
--- math.js
+++ math.js
@@ -1,14 +1,21 @@
 
 // math module 
-function add (a, b){
+export function add (a, b){
     return a + b;
 }
-function subtract (a, b){
+export function subtract (a, b){
     return a - b;
 }
 
+export default function multiply(a, b){ // representing default export
+    return a * b;
+}
+
 //common js export
-module.exports = {
-    add :add,
-    sub :subtract
-};
+// module.exports = {
+//     add :add,
+//     sub :subtract
+// };
+
+// es6 export
+// export { add, subtract };
