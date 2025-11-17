538ff9cecb80f3b70f771ddb90f193f9f4687af2
unknown <jangjaeko45@gmail.com>
2025-11-16 19:46:34 -0800

    feat : node modules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // const { add, sub } = require("./math"); // only common js
 
 // import mul from "./math.js"; // es6 module
-import mul, { add, subtract } from "./math.js"; // es6 module
+// import mul, { add, subtract } from "./math.js"; // es6 module
 
 
 // console.log(moduleData) ; // { add: [Function: add], sub: [Function: subtract] }
@@ -10,6 +10,12 @@ import mul, { add, subtract } from "./math.js"; // es6 module
 // console.log(moduleData.add(2,3)); // 5
 // console.log(moduleData.sub(5,3)); // 2
 
-console.log(add(2, 3)); // 5
-console.log(subtract(5, 3)); // 2
-console.log(mul(2, 3)); // 6
+// console.log(add(2, 3)); // 5
+// console.log(subtract(5, 3)); // 2
+// console.log(mul(2, 3)); // 6
+
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
